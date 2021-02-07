070544da0137e29de6179ec427578ef98532626e
coosir <git@coosir.com>
2021-02-07 22:11:31 +0800

    支持区分不同的环境

--- docs/fabu.dev.sql
+++ docs/fabu.dev.sql
@@ -7,7 +7,7 @@
 #
 # 主机: 127.0.0.1 (MySQL 8.0.13)
 # 数据库: fabu.dev
-# 生成时间: 2021-01-29 07:48:28 +0000
+# 生成时间: 2021-02-07 14:03:10 +0000
 # ************************************************************
 
 
@@ -30,12 +30,12 @@ CREATE TABLE `app` (
   `name` varchar(25) NOT NULL DEFAULT '' COMMENT 'app名称',
   `team_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'app归属的团队ID',
   `platform` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '平台（1：iOS，2：Android）',
+  `env` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '环境（1：正式，2：测试）',
   `icon` varchar(150) NOT NULL DEFAULT '' COMMENT 'app的图标',
   `short_url` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '短链接',
   `bundle_id` varchar(100) NOT NULL DEFAULT '' COMMENT '包名',
   `current_version` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '当前版本号',
   `current_build` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '当前版本数',
-  `identifier` varchar(30) NOT NULL DEFAULT '' COMMENT 'APP KEY',
   `is_public` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否公开（0：否，1：是）',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '2' COMMENT '状态（1：删除，2有效）',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -43,7 +43,7 @@ CREATE TABLE `app` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `created_by` varchar(50) NOT NULL DEFAULT '' COMMENT '添加人',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `uk_platform_bundle_id` (`platform`,`bundle_id`),
+  UNIQUE KEY `uk_platform_bundle_env` (`platform`,`bundle_id`,`env`),
   KEY `idx_team_id` (`team_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='app主表';
 
@@ -59,16 +59,16 @@ CREATE TABLE `app_version` (
   `app_id` bigint(20) unsigned NOT NULL COMMENT 'APP ID',
   `version` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '版本名',
   `build` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '版本数',
-  `description` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '更新说明',
+  `description` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '更新说明',
   `size` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '包大小',
-  `hash` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'hash',
-  `path` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '文件存放路径（如果上传到云平台这里是url）',
+  `hash` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'hash',
+  `path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '文件存放路径（如果上传到云平台这里是url）',
   `is_publish` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否发布（0：未发布，1：发布）',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '2' COMMENT '状态（1：删除，2有效）',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `updated_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `updated_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
+  `created_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `uk_hash` (`hash`),
   KEY `idx_app_id` (`app_id`)
@@ -83,19 +83,19 @@ DROP TABLE IF EXISTS `member`;
 
 CREATE TABLE `member` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `mobile` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机',
-  `account` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '登录账户',
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '姓名',
-  `password` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '密码',
-  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '头像',
-  `email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `mobile` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机',
+  `account` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '登录账户',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '姓名',
+  `password` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '头像',
+  `email` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '邮箱',
   `token` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Token',
-  `api_token` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'API Token',
+  `api_token` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'API Token',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '2' COMMENT '状态（1：删除，2有效）',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `updated_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `updated_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
+  `created_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `uk_token` (`token`) USING BTREE,
   UNIQUE KEY `uk_email` (`email`) USING BTREE,
@@ -124,9 +124,9 @@ CREATE TABLE `team` (
   `name` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '团队名',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '2' COMMENT '状态（1：删除，2有效）',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `updated_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `updated_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
+  `created_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='团队表';
 
@@ -135,8 +135,7 @@ LOCK TABLES `team` WRITE;
 
 INSERT INTO `team` (`id`, `owner`, `name`, `status`, `updated_at`, `updated_by`, `created_at`, `created_by`)
 VALUES
-	(1,1,'测试团队',2,'2020-12-03 18:22:32','','2020-12-03 18:22:32','admin'),
-	(2,1,'测试2',2,'2021-01-21 17:36:38','','2021-01-21 17:36:38','admin');
+	(1,1,'测试团队',2,'2021-02-07 22:02:48','admin','2020-12-03 18:22:32','admin');
 
 /*!40000 ALTER TABLE `team` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -151,11 +150,11 @@ CREATE TABLE `team_member` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `team_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '团队ID',
   `member_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '会员ID',
-  `role` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '角色（1 普通成员 2 管理员 3 所有人）',
+  `role` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '角色（1 普通成员 2 开发者 3 管理员）',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `updated_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
+  `updated_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '修改人',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `created_by` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
+  `created_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '添加人',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `uk_team_member_id` (`team_id`,`member_id`) USING BTREE,
   KEY `idx_member_id` (`member_id`) USING BTREE
@@ -166,8 +165,7 @@ LOCK TABLES `team_member` WRITE;
 
 INSERT INTO `team_member` (`id`, `team_id`, `member_id`, `role`, `updated_at`, `updated_by`, `created_at`, `created_by`)
 VALUES
-	(1,1,1,3,'2020-12-03 18:22:33','','2020-12-03 18:22:33','admin'),
-	(2,2,1,3,'2021-01-21 17:36:38','','2021-01-21 17:36:38','admin');
+	(1,1,1,3,'2021-02-07 22:02:39','','2021-01-21 17:36:38','admin');
 
 /*!40000 ALTER TABLE `team_member` ENABLE KEYS */;
 UNLOCK TABLES;
